06d3bd17e333b04241368c6e6270052492252105
gwc593 <gwc593@gmail.com>
2020-03-03 21:32:58 +0000

    ready for next class on vertex attributes.

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,28 @@ int doMain()
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    //TRIANGLE DEFINITION
+    //
+    float positions[6]{
+            -0.5f, -0.5f,
+             0.0f,  0.5f,
+             0.5f, -0.5f
+    };
+    unsigned int buffer;  //VRAM memery partition id
+    glGenBuffers(1,&buffer);  //make new vertex array and pass its id into buffer ID
+    glBindBuffer(GL_ARRAY_BUFFER,buffer);  //select the new buffer and set its type to a array buffer.
+    glBufferData(GL_ARRAY_BUFFER,6* sizeof(float),positions,GL_STATIC_DRAW);
+
+    // Todo: vertex attributes lesson https://www.youtube.com/watch?v=x0H--CL2tUI&list=PLlrATfBNZ98foTJPJ_Ev03o2oq3-GGOS2&index=5
+
     Time::init();
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        //render nothing.....
+
+        glDrawArrays(GL_TRIANGLES,0,3); // draw selected triangle. (bind another buffer if you want to draw that.
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
